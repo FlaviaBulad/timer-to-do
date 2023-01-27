d20a6ca69974808a6c194dccf4583e3ceb2665b6
Flavia <flavia.bulad@gmail.com>
2023-01-27 09:28:49 -0300

    feat: :art: add timer style

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,11 +1,13 @@
 import { Button } from "../Button";
 import { Clock } from "./Clock";
 
+import "./style.scss";
+
 export const Timer = () => {
   return (
-    <div>
-      <p>Choose a card and start the timer</p>
-      <div>
+    <div className="timer">
+      <p className="title">Choose a card and start the timer</p>
+      <div className="clockWrapper">
         <Clock />
       </div>
       <div>
