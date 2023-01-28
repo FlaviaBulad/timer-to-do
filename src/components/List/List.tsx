813a27b6989590bf12d8d24a8a64f376faffc473
Flavia <flavia.bulad@gmail.com>
2023-01-28 16:35:11 -0300

    feat: :sparkles: add dinamic task list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,28 +1,27 @@
+import { useState } from "react";
 import { Item } from "./Item";
 import "./style.scss";
 
+interface ITask {
+  task: string;
+  time: string;
+}
+
 export const List = () => {
-  const tasks = [
-    {
-      task: "React",
-      time: "02:00:00",
-    },
-    {
-      task: "Javascript",
-      time: "01:00:00",
-    },
-    {
-      task: "Typescript",
-      time: "01:50:00",
-    },
-  ];
+  const [tasks, setTasks] = useState<ITask[]>([]);
 
   return (
     <aside className="task-list">
-      <h2> Today's tasks </h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks]);
+        }}
+      >
+        Today's tasks
+      </h2>
       <ul>
-        {tasks.map((item, index) => (
-          <Item key={index} {...item} />
+        {tasks.map((item) => (
+          <Item key={item.task} {...item} />
         ))}
       </ul>
     </aside>
